7ff119e5f40d1d7f14cc348e8510112789bb6623
Bryce Allen <bdallen@uchicago.edu>
2023-06-19 17:43:52 +0000

    add (-inf, inf) tests, hard code expected
    hard coded expected value is more robust; there is at least one
    case, icpx with fp-model fast, where exp and pow return the same
    wrong answer

--- tests/test_std_complex_exp.cxx
+++ tests/test_std_complex_exp.cxx
@@ -16,7 +16,6 @@ TYPED_TEST(ComplexExpTest, nan_pinf_exp)
 {
   using CT = std::complex<TypeParam>;
 
-  auto ninf = std::numeric_limits<TypeParam>::infinity() * -1;
   auto pinf = std::numeric_limits<TypeParam>::infinity();
   auto nan = std::numeric_limits<TypeParam>::quiet_NaN();
 
@@ -25,6 +24,26 @@ TYPED_TEST(ComplexExpTest, nan_pinf_exp)
 
   auto pow_result = std::pow(x, y);
   auto exp_log_result = std::exp(y * std::log(x));
+  auto expected_result = CT(0, 0);
 
-  EXPECT_EQ(pow_result, exp_log_result);
+  EXPECT_EQ(pow_result, expected_result);
+  EXPECT_EQ(exp_log_result, expected_result);
+}
+
+TYPED_TEST(ComplexExpTest, ninf_pinf_exp)
+{
+  using CT = std::complex<TypeParam>;
+
+  auto ninf = std::numeric_limits<TypeParam>::infinity() * -1;
+  auto pinf = std::numeric_limits<TypeParam>::infinity();
+
+  auto x = CT(4.42, 2.02);
+  auto y = CT(ninf, pinf);
+
+  auto pow_result = std::pow(x, y);
+  auto exp_log_result = std::exp(y * std::log(x));
+  auto expected_result = CT(0, 0);
+
+  EXPECT_EQ(pow_result, expected_result);
+  EXPECT_EQ(exp_log_result, expected_result);
 }
